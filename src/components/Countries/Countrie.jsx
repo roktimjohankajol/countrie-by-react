42a3adbe61b3305966e791019343a2e87a6f7d57
roktimjohankajol <roktim656214@gmail.com>
2023-09-17 21:05:02 +0600

    Code Update

--- src/components/Countries/Countrie.jsx
+++ src/components/Countries/Countrie.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Countrie.css';
-const Countrie = ({countrie}) => {
+const Countrie = ({countrie, handleVisitedCountries}) => {
     console.log(countrie)
 
     const {flags, name, cca3} = countrie;
@@ -8,16 +8,18 @@ const Countrie = ({countrie}) => {
     const [visited, setVisited] = useState(false);
 
     const handleVisited = () => {
-        setVisited(true);
+        setVisited(!visited);
     }
 
+
     return (
-        <div className="countrieStyle">
+        <div className={`countrieStyle ${visited && 'visited'}`}>
                 <img src={flags.png} alt="" />
-                <h2>{name.common}</h2>
+                <h2 style={{color: visited ? 'blue' : 'red'}}>{name.common}</h2>
                 <h2>{cca3}</h2>
+                <button onClick={() => handleVisitedCountries(countrie)}>Mark Visited</button><br />
                 <button onClick={handleVisited}>{visited ? 'Visited' : 'visit'}</button>
-                {visited ? 'I am visited this area' : 'I want visit this area'}
+                {visited ? ' I am visited this area' : ' I want visit this area'}
         </div>
     );
 };
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,18 +4,35 @@ import './Countrie.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]); 
+    const [visitedCountries, setVisitedCountries]= useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[]);
+
+    const handleVisitedCountries = (countrie) => {
+        console.log('add this visited countries')
+        // console.log(countrie)
+        const newVisitedCountries = [...visitedCountries, countrie]; 
+        setVisitedCountries(newVisitedCountries);
+    }
     return (
         <div>
             <h1>Roktim Johan Kajol : {countries.length}</h1>
+            <h2>Visited Countrie list : {visitedCountries.length} </h2>
+            <ul className="flagStyle">
+                {
+                    visitedCountries.map( countrie => <img src={countrie.flags.png}></img>)
+                }
+            </ul>
             <div className="countrie-container">
                 {
-                    countries.map(countrie => <Countrie key={countrie.cca3} countrie={countrie}></Countrie>)
+                    countries.map(countrie => <Countrie key={countrie.cca3} 
+                        countrie={countrie}
+                        handleVisitedCountries={handleVisitedCountries}
+                        ></Countrie>)
                 }
             </div>
         </div>
